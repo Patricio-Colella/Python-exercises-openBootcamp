2e82a8e754e5ca9c80ac1b050b3baf1bad743007
patricio colella <patriciogabrielcolella@gmail.com>
2022-10-06 16:53:04 -0300

    ejercicio 5

---
+++ ejercicio 5/ejercicio 5.py
@@ -0,0 +1,10 @@
+dias = input("ingrese la cantidad de dias de febrero:")
+ultimo_año = input("hace cuantos años fue el ultimo año bisiesto?:")
+
+
+if dias=="29" and ultimo_año=="4":
+    print("es año bisiesto")
+else:
+    print("no es año bisiesto")
+
+input()
