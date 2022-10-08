7e4e023b4eff6c3f11030a3c71225bc50aea17d7
patricio colella <patriciogabrielcolella@gmail.com>
2022-10-08 15:02:16 -0300

    ejercicio 7

---
+++ ejercicio 7/ejercicio 7.py
@@ -0,0 +1,17 @@
+import calculadora as Math
+
+
+n1 = int(input("ingrese nº1: "))
+n2 = int(input("ingrese nº2: "))
+
+
+print("suma (",n1,"+",n2,")=",Math.suma(n1,n2))
+
+print("resta (",n1,"-",n2,")=",Math.resta(n1,n2))
+
+print("multiplicacion (",n1,"*",n2,")=",Math.multiplicacion(n1,n2))
+
+print("division (",n1,"/",n2,")=",Math.division(n1,n2))
+
+input()
+
